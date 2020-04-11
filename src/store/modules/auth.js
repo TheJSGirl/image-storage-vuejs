94d206917865d8cfcf1d11aa2fd6870b6f7b79aa
TheJSGirl <prinegi100@gmail.com>
2020-04-12 02:07:29 +0530

    feat: persist localstorage token

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,7 +2,7 @@ import api from '../../api/imgur';
 import qs from 'qs';
 
 const state = {
-  token: null,
+  token: window.localStorage.getItem('imgur_token'),
 };
 
 const getters = {
@@ -16,6 +16,7 @@ const actions = {
   finalizeLogin: ({ commit },hash) => {
     const query = qs.parse(hash.replace('#', ''));
     commit('setToken', query.access_token);
+    window.localStorage.setItem('imgur_token', query.access_token)
   },
   logout: ({commit}) => {
     commit('setToken', null);
