173e236d5af2b34e359d6cb01249f69b1ee38472
TheJSGirl <prinegi100@gmail.com>
2020-04-12 02:45:43 +0530

    feat: redirect to home

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,5 +1,6 @@
 import api from '../../api/imgur';
 import qs from 'qs';
+import { router } from '../../main';
 
 const state = {
   token: window.localStorage.getItem('imgur_token'),
@@ -16,7 +17,8 @@ const actions = {
   finalizeLogin: ({ commit },hash) => {
     const query = qs.parse(hash.replace('#', ''));
     commit('setToken', query.access_token);
-    window.localStorage.setItem('imgur_token', query.access_token)
+    window.localStorage.setItem('imgur_token', query.access_token);
+    router.push('/');
   },
   logout: ({commit}) => {
     commit('setToken', null);
