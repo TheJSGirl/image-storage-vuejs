5fd2f9dff2330821300ed3b03748811d3963df00
TheJSGirl <prinegi100@gmail.com>
2020-04-12 00:29:12 +0530

    feat: added login button & connected oauth flow

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
+
+    <div class="right menu">
+      <a href="#" class="ui item" @click="login">Login</a>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  methods: mapActions(['login']),
+    // methods: {
+  //   // using spread operater you can add other methods 
+  //   ...mapActions(['login']),
+
+  // }
 }
 </script>
