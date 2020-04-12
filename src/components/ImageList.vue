96312e9e17795281a2d452a4e83ce8351660b1a9
TheJSGirl <prinegi100@gmail.com>
2020-04-12 14:47:46 +0530

    feat: displayed imagelist

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
-    Imgae list
+  <div class="ui medium images custom-image">
+    <img  class="ui image" v-for="(image, index) in allImages" :key="index" :src="image.link" />
   </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex';
+import { mapActions, mapGetters} from 'vuex';
 
 export default {
   name: 'ImageList',
+  computed: mapGetters(['allImages']),
   methods: mapActions(['fetchImages']),
   created() {
     this.fetchImages();
   }
 };
 </script>
+
+<style scoped>
+  .custom-image {
+    margin: 10px;
+  }
+</style>
